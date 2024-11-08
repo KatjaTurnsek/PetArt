1e01f23781f6849fd0220da3b001efb69fe44331
Katja Turnsek <158604683+KatjaTurnsek@users.noreply.github.com>
2024-11-08 17:53:10 +0100

    edits

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -1,28 +1,40 @@
-let slideIndex = 1;
-
-function openLightbox() {
-  document.getElementById('lightbox').style.display = 'block';
-  showSlide(slideIndex);
+// Open the Modal
+function openModal() {
+  document.getElementById("myModal").style.display = "block";
 }
 
-function closeLightbox() {
-  document.getElementById('lightbox').style.display = 'none';
+// Close the Modal
+function closeModal() {
+  document.getElementById("myModal").style.display = "none";
 }
 
-function changeSlide(n) {
-  showSlide(slideIndex += n);
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
 }
 
-function setCurrentSlide(n) {
-  showSlide(slideIndex = n);
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
 }
 
-function showSlide(n) {
-  const slides = document.getElementsByClassName('lightbox-image');
-  if (n > slides.length) { slideIndex = 1; }
-  if (n < 1) { slideIndex = slides.length; }
-  for (let i = 0; i < slides.length; i++) {
-    slides[i].style.display = 'none';
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  var captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
   }
-  slides[slideIndex - 1].style.display = 'block';
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
 }
