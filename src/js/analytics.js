3cc7983528ace7b238067b9cdd2454cd62c4f8ae
Katja Turnsek <158604683+KatjaTurnsek@users.noreply.github.com>
2024-12-30 09:59:24 +0100

    analytics js file

---
+++ src/js/analytics.js
@@ -0,0 +1,13 @@
+(function() {
+  var script = document.createElement('script');
+  script.async = true;
+  script.src = 'https://www.googletagmanager.com/gtag/js?id=G-4BP4G6562F';
+  document.head.appendChild(script);
+
+  window.dataLayer = window.dataLayer || [];
+  function gtag() {
+    dataLayer.push(arguments);
+  }
+  gtag('js', new Date());
+  gtag('config', 'G-4BP4G6562F');
+})();
